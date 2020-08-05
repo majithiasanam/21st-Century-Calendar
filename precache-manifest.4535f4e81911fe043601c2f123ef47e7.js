59283253a87e2599ab3d156ec7b2bdd62bdc836a
Sanam MAJITHIA <sanam.majithia@amadeus.com>
2020-08-05 20:46:35 +0530

    Updates

---
+++ precache-manifest.4535f4e81911fe043601c2f123ef47e7.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c60cf756e6871e2858e170fef21a72f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "29b79c075364991e7719",
+    "url": "/static/css/main.7bb80df5.chunk.css"
+  },
+  {
+    "revision": "40b4326ddf67469a2ce0",
+    "url": "/static/js/2.9635763f.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.9635763f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29b79c075364991e7719",
+    "url": "/static/js/main.922341ca.chunk.js"
+  },
+  {
+    "revision": "9fd970e1f25c0cd7eb0a",
+    "url": "/static/js/runtime-main.470b33ba.js"
+  },
+  {
+    "revision": "664aa5d7a9d3f8820616a6e17e768d32",
+    "url": "/static/media/friday.664aa5d7.svg"
+  },
+  {
+    "revision": "6af1656621e0c44624f50c7167eaf416",
+    "url": "/static/media/monday.6af16566.svg"
+  },
+  {
+    "revision": "64d0c4b5a244b566361cb650f12f6e2c",
+    "url": "/static/media/saturday.64d0c4b5.svg"
+  },
+  {
+    "revision": "0256b07845db59084042c687e78e76f3",
+    "url": "/static/media/sunday.0256b078.svg"
+  },
+  {
+    "revision": "76a23e166e997b60637235f1cb1d686e",
+    "url": "/static/media/thursday.76a23e16.svg"
+  },
+  {
+    "revision": "c5117fae1f7070d7294b1c1dcba76ac1",
+    "url": "/static/media/tuesday.c5117fae.svg"
+  },
+  {
+    "revision": "686bfb6b757c5dc8102b7d65fe5f6163",
+    "url": "/static/media/wednesday.686bfb6b.svg"
+  }
+]);
